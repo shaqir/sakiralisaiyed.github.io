f984fd366b1460c66de175e3d1f9bb718e516178
SakirAli Saiyed <SakirSaiyed29@gmail.com>
2025-09-05 23:46:50 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 // astro.config.mjs
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import tailwind from '@tailwindcss/vite';
 
 export default defineConfig({
   site: 'https://sakiralisaiyed.com', // or your github.io URL
-  integrations: [tailwind()],
+  vite: {
+    plugins: [tailwind()],
+  },
 });
