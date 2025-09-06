2bf139e293c3de9dbff2ef97151212ea6df9cb6e
SakirAli Saiyed <SakirSaiyed29@gmail.com>
2025-09-05 23:53:38 -0600

    Delete tailwind.config.mjs

--- tailwind.config.mjs
+++
@@ -1,7 +0,0 @@
-// tailwind.config.mjs
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ['./src/**/*.{astro,html,js,jsx,ts,tsx,md,mdx}'],
-  theme: { extend: {} },
-  plugins: [],
-};
