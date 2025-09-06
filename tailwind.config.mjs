153f681f236824c0d6f27206be6959a224a9424e
SakirAli Saiyed <SakirSaiyed29@gmail.com>
2025-09-05 23:40:39 -0600

    Create tailwind.config.mjs

---
+++ tailwind.config.mjs
@@ -0,0 +1,7 @@
+// tailwind.config.mjs
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,ts,tsx,md,mdx}'],
+  theme: { extend: {} },
+  plugins: [],
+};
